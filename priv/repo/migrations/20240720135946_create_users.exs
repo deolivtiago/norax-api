05deae622f0c701609b74daeb34b7ec29fc86f44
Tiago de Oliveira <deoliv.tiago@gmail.com>
2024-07-21 00:05:40 +1000

    feat: add migration to create users table

---
+++ priv/repo/migrations/20240720135946_create_users.exs
@@ -0,0 +1,25 @@
+defmodule NoraxCore.Repo.Migrations.CreateUsers do
+  use Ecto.Migration
+
+  def change do
+    create table(:users, primary_key: false) do
+      add :id, :binary_id, primary_key: true
+
+      add :first_name, :string, null: false
+      add :last_name, :string, default: ""
+
+      add :email, :string, null: false
+      add :password, :string, null: false
+
+      add :avatar_url, :string, default: ""
+      add :role, :string, null: false
+
+      add :otp_secret, :binary, null: false
+      add :is_verified, :boolean, default: false
+
+      timestamps(type: :timestamptz)
+    end
+
+    create unique_index(:users, [:email])
+  end
+end
