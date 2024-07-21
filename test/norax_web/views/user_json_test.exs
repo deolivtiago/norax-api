c3d39e35fe681f514afc16c6bd803c6aa3ab52ce
Tiago de Oliveira <deoliv.tiago@gmail.com>
2024-07-21 15:51:12 +1000

    test: ensure json renders user data

---
+++ test/norax_web/views/user_json_test.exs
@@ -0,0 +1,35 @@
+defmodule NoraxWeb.UserJSONTest do
+  use NoraxWeb.ConnCase, async: true
+
+  import NoraxCore.Accounts.UsersFixtures
+
+  alias NoraxWeb.UserJSON
+
+  setup do
+    {:ok, user: build_user()}
+  end
+
+  describe "renders" do
+    test "a list of users", %{user: user} do
+      assert %{data: [user_data]} = UserJSON.index(%{users: [user]})
+
+      assert user_data.id == user.id
+      assert user_data.first_name == user.first_name
+      assert user_data.last_name == user.last_name
+      assert user_data.email == user.email
+      assert user_data.avatar_url == user.avatar_url
+      assert user_data.role == user.role
+    end
+
+    test "a single user", %{user: user} do
+      assert %{data: user_data} = UserJSON.show(%{user: user})
+
+      assert user_data.id == user.id
+      assert user_data.first_name == user.first_name
+      assert user_data.last_name == user.last_name
+      assert user_data.email == user.email
+      assert user_data.avatar_url == user.avatar_url
+      assert user_data.role == user.role
+    end
+  end
+end
