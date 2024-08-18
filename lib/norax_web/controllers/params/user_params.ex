e5ff8dc14ab699649524f76d4cc170de074c9cc9
Tiago de Oliveira <deoliv.tiago@gmail.com>
2024-08-18 13:07:36 +1000

    feat: add params to validate users request

---
+++ lib/norax_web/controllers/params/user_params.ex
@@ -0,0 +1,29 @@
+defmodule NoraxWeb.Controllers.UserParams do
+  @moduledoc false
+  use NoraxWeb, :params
+
+  defparams :create do
+    required(:first_name, :string, min: 2, max: 255, squish: true)
+    optional(:last_name, :string, max: 255, squish: true)
+
+    required(:email, :string, format: :email, min: 3, max: 160, trim: true)
+    required(:password, :string, format: :password, min: 6, max: 72, trim: true)
+
+    optional(:avatar_url, :string, format: :url, max: 255)
+    optional(:role, :enum, values: ~w(user admin))
+  end
+
+  defparams :get do
+    required(:id, :uuid)
+  end
+
+  defparams :update do
+    required(:first_name, :string, min: 2, max: 255, squish: true)
+    optional(:last_name, :string, max: 255, squish: true)
+
+    required(:email, :string, format: :email, min: 3, max: 160, trim: true)
+
+    optional(:avatar_url, :string, format: :url, max: 255)
+    optional(:role, :enum, values: ~w(user admin))
+  end
+end
