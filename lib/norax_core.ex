bd9d343bfb90a429f409b3f7232cb65645c7c073
Tiago de Oliveira <deoliv.tiago@gmail.com>
2024-08-18 11:55:18 +1000

    feat: add schema to validate user attrs

--- lib/norax_core.ex
+++ lib/norax_core.ex
@@ -5,5 +5,38 @@ defmodule NoraxCore do
 
   Contexts are also responsible for managing your data, regardless
   if it comes from the database, an external API or others.
+
+  This can be used in your application as:
+
+      use NoraxCore, :schema
+      use NoraxCore, :validation
+
   """
+
+  def schema do
+    quote do
+      use Ecto.Schema
+
+      unquote(validation())
+
+      @primary_key {:id, :binary_id, autogenerate: true}
+      @foreign_key_type :binary_id
+
+      def changeset, do: %{change(struct(__MODULE__)) | valid?: false}
+    end
+  end
+
+  def validation do
+    quote do
+      import Ecto.Changeset
+
+      defp cast(schema, params) when is_non_struct_map(schema) or is_list(schema) do
+        %{Goal.build_changeset(schema, params) | valid?: false}
+      end
+    end
+  end
+
+  defmacro __using__(which) when is_atom(which) do
+    apply(__MODULE__, which, [])
+  end
 end
---
+++ lib/norax_core/accounts/users/user.ex
@@ -0,0 +1,47 @@
+defmodule NoraxCore.Accounts.Users.User do
+  @moduledoc """
+  `User` schema
+  """
+  use NoraxCore, :schema
+
+  alias __MODULE__
+
+  schema "users" do
+    field :first_name, :string
+    field :last_name, :string, default: ""
+
+    field :email, :string
+    field :password, :string, redact: true
+
+    field :avatar_url, :string, default: ""
+    field :role, Ecto.Enum, values: ~w(user admin)a, default: :user
+
+    timestamps(type: :utc_datetime)
+  end
+
+  @doc false
+  def changeset(user \\ %User{}, params) when is_map(params) do
+    required_fields = ~w(first_name email password)a
+    optional_fields = ~w(last_name avatar_url role)a
+
+    user
+    |> cast(params, required_fields ++ optional_fields)
+    |> validate_required(required_fields)
+    |> unique_constraint(:id, name: :users_pkey)
+    |> validate_length(:first_name, min: 2, max: 255)
+    |> validate_length(:last_name, max: 255)
+    |> unique_constraint(:email, name: :users_email_index)
+    |> update_change(:email, &String.downcase/1)
+    |> validate_length(:email, min: 3, max: 160)
+    |> validate_format(:email, ~r/^[.!?@#$%^&*_+a-z\-0-9]+[@][._+\-a-z0-9]+$/)
+    |> validate_length(:password, min: 6, max: 72)
+    |> validate_length(:password, max: 72, count: :bytes)
+    |> validate_format(:password, ~r/[0-9]/, message: "must have number(s)")
+    |> validate_format(:password, ~r/[a-z]/, message: "must have lower case character(s)")
+    |> validate_format(:password, ~r/[A-Z]/, message: "must have upper case character(s)")
+    |> validate_format(:password, ~r/[.!?@#$%^&*_+\-]/, message: "must have special character(s)")
+    |> update_change(:password, &Argon2.hash_pwd_salt/1)
+    |> update_change(:avatar_url, &String.downcase/1)
+    |> validate_length(:avatar_url, max: 255)
+  end
+end
